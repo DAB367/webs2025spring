123abd39dea0f170e3a02656b30b46556eda73a7
Ladymoon2886 <danabriggs86@gmail.com>
2025-03-29 19:53:14 -0400

    Add image rotation
    Add image rotation and explore using svelte.

---
+++ webs2025spring/src/routes/hobbies/+page.svelte
@@ -0,0 +1,9 @@
+<svelte:head>
+	<title>Hobbies</title>
+	<meta name="description" content="About my hobbies" />
+</svelte:head>
+
+<div class="text-column">
+	<h1>About my hobbies</h1>
+
+</div>
---
+++ webs2025spring/src/routes/projects/+page.svelte
@@ -0,0 +1,36 @@
+<script>
+import Showcase from "../../components/showcase.svelte";
+</script>
+<svelte:head>
+	<title>Projects</title>
+	<meta name="description" content="About my projects" />
+</svelte:head>
+
+<div class="text-column">
+	<h1>About my Projects</h1>
+
+</div>
+<Showcase>
+    
+</Showcase>
+    <div>
+          <img src="Projects/BeachPic.jpg" alt="Beach at sunrise with seagulls flying over waves">
+          <audio controls><source  src="Projects/BeachConversation.mp3" type="audio/mpeg"/></audio>
+    </div>
+    <div>
+
+        <h3>A Beach Conversion</h3>
+        <p>This is an audio narrative project that I completed for my Composing Digital Media class in Spring 2025.</p>
+        <p>For more information on my project please <a href="https://github.com/DAB367/audio-narrative-2025spring">Visit my AudioNarrative Github Repo.</a></p>
+    </div> 
+
+
+    <div>
+          <img src="Projects/AsFragileAsASpidersWeb.jpg" alt="A spider's web with the Earth at it's center and fire starting to engulf it.">
+    </div>
+    <div>
+
+        <h3>As Fragile as a spider's web</h3>
+        <p>This is a visual argument project that I completed for my Composing Digital Media class in Spring 2025.</p>
+        <p>For more information on my project please <a href="https://github.com/DAB367/visual-argument-2025spring">Visit my Visual Argument Github Repo.</a></p>
+    </div> 
