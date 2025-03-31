b83aef7e89e63e672f7c1e58a1c69d98718640de
Ladymoon2886 <danabriggs86@gmail.com>
2025-03-31 16:10:34 -0400

    Add Headshot to Webpages
    Add Headshot to all webpages

--- docs/js/script.js
+++ docs/js/script.js
@@ -2,7 +2,8 @@
 let slideIndex = 0;
 showSlides();
 
-function showSlides() {
+function showSlides() 
+{
   let i;
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("dot");
---
+++ webs2025spring/src/components/showcase.svelte
@@ -0,0 +1,11 @@
+<script>
+	import beach from './BeachPic.jpg';
+    import fragile from './AsFragileAsASpidersWeb.jpg';
+    import audio from './BeachConversation.mp3';
+</script>
+
+<img alt="The Beach at sunrise with seagulls flying over waves" src={beach} />
+
+<div>
+    Temp Text
+</div>
