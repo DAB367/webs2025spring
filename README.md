ab521667d334af7197eaa2d3be8c7162f4ad5218
Ladymoon2886 <danabriggs86@gmail.com>
2025-04-06 10:53:45 -0400

    Clean up files and Add pictures
    Clean up files
    Removed unnecessary files
    Add pictures to Hobbies

--- README.md
+++ README.md
@@ -10,3 +10,4 @@ To visit live webpage [please click here](https://dab367.github.io/webs2025sprin
 |An image of a red and black substance Photo from Ayush Kumar|[Unsplash](https://unsplash.com/photos/an-image-of-a-red-and-black-substance-9Njoam3Vesc)|[Unsplash License](https://unsplash.com/license)|
 |Water droplets on a surface Photo by D koi|[Unsplash](https://unsplash.com/photos/water-droplets-on-a-surface-5nI9N2wNcBU)|[Unsplash License](https://unsplash.com/license)|
 |Beach Photo by Frank Mckenna|[Unsplash](https://unsplash.com/photos/five-birds-flying-on-the-sea-OD9EOzfSOh0)|[Unsplash License](https://unsplash.com/license)|
+|GitHub Logo|[GitHub](https://github.com/logos)|[GitHub Logo License](https://github.com/logos)|
--- docs/js/script.js
+++ docs/js/script.js
@@ -3,8 +3,23 @@ let pictures = [
     "img/Waimea1.jpg"
 ]
 
-const hoverOverCardTemplate = <img id="crochet" class="mouseover-img" src="img/crochet.jpg"
-alt="Overhead view of a crochet shawl on top of a flower blanket">
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
+
+const hoverOverCardTemplate = <img id="crochet" class="mouseover-img" src="img/crochet.jpg" alt="Overhead view of a crochet shawl on top of a flower blanket">
 <div class="overlay">
 <div class="text">
     <h3>
@@ -42,55 +57,3 @@ alt="Overhead view of a crochet shawl on top of a flower blanket">
 <p>Crocheting is super fun!</p>
 </div>;
 
-window.addEventListener("load", (event) => {
-    console.log("page is fully loaded");
-});
-
-
-window.onload = (event) => {
-    let parentCard = document.querySelector('#carouselContainer')
-    console.log("page is fully loaded");
-
-    let track = document.querySelector('.carousel_track');
-    let slides = Array.from(track.children)
-    let slideSize = slides[0].getBoundingClientRect();
-    let slideWidth = slideSize.width;
-
-    let dotsNav = document.querySelector('.carousel_nav');
-    let currentDot = dotsNav.querySelector('.current-slide');
-    let dots = Array.from(dotsNav.children);
-    let currentSlide = track.querySelector('.current-slide');
-
-    let nextButton = document.querySelector('.carousel_button--right');
-    let prevButton = document.querySelector('.carousel_button--left');
-
-    //arrange the slides next to each other
-    const setSlidePosition = (slide, index) => {
-        slide.style.left = slideWidth * index + 'px';
-    };
-    slides.forEach(setSlidePosition);
-
-    const moveToSlide = (track, currentSlide, targetSlide) => {
-        track.style.transform = 'translateX(-' + targetSlide.style.left + ')';
-        currentSlide.classList.remove('current-slide');
-        targetSlide.classList.add('current-slide');
-    }
-
-    const updateDots = (currentDot, targetDot) => {
-        currentDot.classList.remove('current-slide');
-        targetDot.classList.add('current-slide');
-    }
-    //when I click the nav indicators, move to that slide
-    dotsNav.addEventListener('click', e => {
-        //what indicator was clicked
-        const targetDot = e.target.closest('button');
-        if (!targetDot) return;
-        currentSlide = track.querySelector('.current-slide');
-        currentDot = dotsNav.querySelector('.current-slide');
-        const targetIndex = dots.findIndex(dot => dot === targetDot);
-        const targetSlide = slides[targetIndex];
-
-        moveToSlide(track, currentSlide, targetSlide);
-        updateDots(currentDot, targetDot);
-    })
-};
